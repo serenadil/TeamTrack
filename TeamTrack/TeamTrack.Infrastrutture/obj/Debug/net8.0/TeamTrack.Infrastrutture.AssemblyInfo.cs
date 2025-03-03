479d79c6520569fe5c1097aa21898934dba7d324
Sara <sara.ventura@studenti.unicam.it>
2025-03-03 17:05:46 +0100

    Modifiche

--- TeamTrack/TeamTrack.Infrastrutture/obj/Debug/net8.0/TeamTrack.Infrastrutture.AssemblyInfo.cs
+++ TeamTrack/TeamTrack.Infrastrutture/obj/Debug/net8.0/TeamTrack.Infrastrutture.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeamTrack.Infrastrutture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d41d213e5e6fc00739ff782a9b6ece2e7eeeea6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48691720ac74c3893f148d5d08cb9f05a193501a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeamTrack.Infrastrutture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeamTrack.Infrastrutture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TeamTrack/TeamTrack.Web/obj/Debug/net8.0/TeamTrack.Web.AssemblyInfo.cs
+++ TeamTrack/TeamTrack.Web/obj/Debug/net8.0/TeamTrack.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeamTrack.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d41d213e5e6fc00739ff782a9b6ece2e7eeeea6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48691720ac74c3893f148d5d08cb9f05a193501a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeamTrack.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeamTrack.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
